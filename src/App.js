feaae8cad399149782aec513211068418c979622
shaanthenapster <shaanthenapster@gmail.com>
2021-05-05 19:14:24 +0530

    mdb5 bootstrap module added
    Signed-off-by: shaanthenapster <shaanthenapster@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import crausal_2 from '../src/images/crausal_2.jpg'
+import crausal_3 from '../src/images/crausal_3.jpg'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Image from 'react-bootstrap/Image'
+import { Button } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Carousel from 'react-bootstrap/Carousel'
+import React from "react"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container class="fluid">
+      <Row>
+        <Navbar fluid bg="primary" variant="dark" >
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+        </Navbar>
+      </Row>
+      <Row>
+        <Carousel>
+          <Carousel.Item >
+            <Image
+              className="d-block w-100"
+              src={crausal_2}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image
+              className="d-block w-100"
+              src={crausal_3}
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image
+              className="d-block w-100"
+              src={crausal_2}
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Row>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import '@fortawesome/fontawesome-free/css/all.min.css'; 
+// import 'bootstrap-css-only/css/bootstrap.min.css'; 
+// import 'mdbreact/dist/css/mdb.css';
+import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
